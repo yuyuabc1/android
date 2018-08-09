73951e255a9ad7fe5ba889afdff23c60186cc9ba
zhuchenliang <zhuchenliang@91zhiyin.com>
2018-08-09 16:42:42 +0800

    a

---
+++ App.js
@@ -0,0 +1,98 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text, Image, View } from 'react-native';
+
+class Greeting extends Component {
+    constructor (props) {
+        super(props);
+        this.state = { showText: true };
+    }
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState(previousState => {
+              return { showText: !previousState.showText };
+            });
+          }, 1000);
+    }
+
+
+
+    render () {
+        let display = this.state.showText ? `hello, ${this.props.name}!` : '';
+        return (
+            <Text>{display}</Text>
+        )
+    }
+}
+
+export default class HelloworldApp extends Component {
+    render () {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return (
+            <View>
+                <Text>hello world</Text>
+                <Image source={pic} style={{width: 193, height: 110}} />
+                <Greeting name='www' />
+            </View>
+        );
+    }
+}
+//import {
+//  Platform,
+//  StyleSheet,
+//  Text,
+//  View
+//} from 'react-native';
+//
+//const instructions = Platform.select({
+//  ios: 'Press Cmd+R to reload,\n' +
+//    'Cmd+D or shake for dev menu',
+//  android: 'Double tap R on your keyboard to reload,\n' +
+//    'Shake or press menu button for dev menu',
+//});
+//
+//type Props = {};
+//export default class App extends Component<Props> {
+//  render() {
+//    return (
+//      <View style={styles.container}>
+//        <Text style={styles.welcome}>
+//          Welcome to React Native!
+//        </Text>
+//        <Text style={styles.instructions}>
+//          To get started, edit App.js
+//        </Text>
+//        <Text style={styles.instructions}>
+//          {instructions}
+//        </Text>
+//      </View>
+//    );
+//  }
+//}
+//
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    justifyContent: 'center',
+//    alignItems: 'center',
+//    backgroundColor: '#F5FCFF',
+//  },
+//  welcome: {
+//    fontSize: 20,
+//    textAlign: 'center',
+//    margin: 10,
+//  },
+//  instructions: {
+//    textAlign: 'center',
+//    color: '#333333',
+//    marginBottom: 5,
+//  },
+//});
